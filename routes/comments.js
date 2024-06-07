60f9c1412f8f5a6c4392f4b3a40e37e6604d5495
J-Jaeh <1wo2gur@naver.com>
2024-06-07 15:58:18 +0900

    feat : #12 댓글 수정 추가

--- routes/comments.js
+++ routes/comments.js
@@ -1,50 +1,63 @@
-import express from 'express';
-import Comment from '../schemas/comment.js';
-import Post from '../schemas/post.js';
+import express from 'express'
+import Comment from '../schemas/comment.js'
+import Post from '../schemas/post.js'
 
 
-const router = express.Router();
+const router = express.Router()
 
 /**
  * 댓글 조회
  */
 router.get('/:postId', async (req, res) => {
-  const {postId} = req.params;
-  const findPostId = await Post.findById({_id:postId},{_id:1}).exec()
+  const { postId } = req.params
+  const findPostId = await Post.findById({ _id: postId }, { _id: 1 }).exec()
 
-  if(!findPostId) return res.status(400).json({success:false,errorMessage:'Post not found'});
+  if (!findPostId) return res.status(400).json({ success: false, errorMessage: 'Post not found' })
 
-  const findComments = await Comment.find({postId: postId},{content:1,createdAt:1})
+  const findComments = await Comment.find({ postId: postId }, { content: 1, createdAt: 1 })
     .sort({ createdAt: -1 })
     .exec()
-  return res.status(200).json({success: true, findComments: findComments})
+  return res.status(200).json({ success: true, findComments: findComments })
 })
-
 
 
 /**
  * 댓글작성
  */
 router.post('/:postId', async (req, res) => {
-  const { content } = req.body;
-  const { postId } =req.params;
-  if(!content) return res.status(400).json({success: false,errorMessage: '댓글 내용을 입력해주세요'});
+  const { content } = req.body
+  const { postId } = req.params
+  if (!content) return res.status(400).json({ success: false, errorMessage: '댓글 내용을 입력해주세요' })
 
-  const findPostId = await Post.findById({_id:postId},{_id:1}).exec()
+  const findPostId = await Post.findById({ _id: postId }, { _id: 1 }).exec()
 
-  if(!findPostId) return res.status(400).json({success:false,errorMessage:'Post not found'});
+  if (!findPostId) return res.status(400).json({ success: false, errorMessage: 'Post not found' })
 
-  const comment = new Comment({content:content,postId:postId})
+  const comment = new Comment({ content: content, postId: postId })
 
   await comment.save()
 
-  return res.status(200).json({success: true,comment_id : comment._id})
+  return res.status(200).json({ success: true, comment_id: comment._id })
 })
 
 
+/**
+ * 댓글 수정
+ */
+router.patch('/:commentId', async (req, res) => {
+  const { commentId } = req.params
+  const { reqContent } = req.body
+
+  if (!reqContent) return res.status(400).json({ success: false, errorMessage: '댓글 내용을 입력해주세요' })
+
+  const findComment = await Comment.findById({ _id: commentId }).exec()
+
+  findComment.content = reqContent
+
+  await findComment.save()
+  return res.status(200).json({ success: true, comment_id: commentId })
+})
 
 
-
-
-export default router;
+export default router
 
