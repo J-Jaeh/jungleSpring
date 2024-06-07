b54199be65ff07d704b53cfc0d213fbc60384a23
J-Jaeh <1wo2gur@naver.com>
2024-06-07 15:49:24 +0900

    feat : #12 댓글 조회추가

--- routes/comments.js
+++ routes/comments.js
@@ -5,9 +5,20 @@ import Post from '../schemas/post.js';
 
 const router = express.Router();
 
+/**
+ * 댓글 조회
+ */
+router.get('/:postId', async (req, res) => {
+  const {postId} = req.params;
+  const findPostId = await Post.findById({_id:postId},{_id:1}).exec()
 
+  if(!findPostId) return res.status(400).json({success:false,errorMessage:'Post not found'});
 
-
+  const findComments = await Comment.find({postId: postId},{content:1,createdAt:1})
+    .sort({ createdAt: -1 })
+    .exec()
+  return res.status(200).json({success: true, findComments: findComments})
+})
 
 
 
