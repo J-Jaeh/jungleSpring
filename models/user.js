4eaf7a23c79f826dfd67d3c5b048ea644f198cd0
J-Jaeh <1wo2gur@naver.com>
2024-06-09 18:18:26 +0900

    feat : #23 서비스 로직 분리 - 유저

--- models/user.js
+++ models/user.js
@@ -1,24 +1,30 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  nickname:{
-    type:String,
-    required:[true,'닉네임은 필수로 입력해야합니다'],
-    unique:true,
-    minlength:[3,'닉네임은 최소 3자 이상이여야 합니다'],
-    match:[/^[a-zA-Z0-9]+$/,'닉네임은 알파벳 대소문자(a~z, A~Z)와 숫자(0~9)로만 구성되어야 합니다.']},
-  password:{
-    type:String,
-    required: true,
-    minlength:4,
-    validate:{
-      validator: function(value){
-        return !value.includes(this.nickname);
-      },
-      message : '비밀번호에는 닉네임을 포함할 수 없습니다.'
-    }
+  nickname: {
+    type: String,
+    required: [true, '닉네임은 필수로 입력해야합니다'],
+    unique: true,
+    minlength: [3, '닉네임은 최소 3자 이상이여야 합니다'],
+    match: [/^[a-zA-Z0-9]+$/, '닉네임은 알파벳 대소문자(a~z, A~Z)와 숫자(0~9)로만 구성되어야 합니다.'],
   },
-}, { timestamps: true });
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+}, { timestamps: true })
 
 
-export default mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema)
+
+export class UserModel {
+  async registerUser(registerUser) {
+    const user = new User(registerUser)
+    return await user.save()
+  }
+
+  async findUser(nickname) {
+    return User.findOne({ nickname: nickname }).exec()
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import User from '../models/user.js'
-import { SECRETKEY } from '../app.js'
+import { generateJwtToken, comparePassword,hashedPassword } from '../utils/auth-util.js'
+import { UserService } from '../services/userService.js'
 
 const router = express.Router()
 
+const userService = new UserService()
 
 /**
  * 회원가입
@@ -15,25 +14,25 @@ router.post('/register', async (req, res) => {
   if (checkPassword !== password) {
     return res.status(400).send({ error: 'Passwords do not match' })
   }
-  if(password.includes(nickname)) {
+  if (password.includes(nickname)) {
     return res.status(400).send({ error: '비밀번호에는 닉네임을 포함할 수 없습니다' })
   }
-
-
-  ///어차피 예외를 db 조회할때 처리할건데 ..
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const user = new User({ nickname: nickname, password: hashedPassword })
+  hashedPassword(res,password)
+  const hashPassword  = await res.locals.hashedPassword
   try {
-    await user.save()
+    const user = await userService.registerUser({ nickname: nickname, password:hashPassword  })
+    console.log("user=",user)
+    console.log("userId =",user._id)
     res.status(200).send({ success: true, user_id: user._id })
   } catch (err) {
-    if (err.code === 11000) {
+    if (err.code === 11000)
       return res.status(409).send({ success: false, errorMessage: 'User already exists' })
-    }
     if (err.name === 'ValidationError') {
       console.error('ValidationError')
       return res.status(400).send({ success: false, errorMessage: err.message })
     }
+    console.error('Error')
+    return res.status(400).send({ success: false, errorMessage: err.message })
   }
 })
 
@@ -42,28 +41,14 @@ router.post('/register', async (req, res) => {
  */
 router.post('/login', async (req, res) => {
   const { nickname, password } = req.body
-
-  const user = await User.findOne({ nickname: nickname }).exec()
-  if (!user) return res.status(401).send({
-    success: false,
-    errorMessage: 'Incorrect Username or Password, or Unregistered User',
-  })
-
-  //비동기함수라 await 걸어야함
-  const isSame = await bcrypt.compare(password, user.password)
-
-  if (!isSame) {
-    return res.status(401).send({
-      success: false,
-      errorMessage: 'Incorrect Username or Password, or Unregistered User',
-    })
+  try {
+    const loginUser= await userService.loginUser({ nickname: nickname, password: password })
+    comparePassword(password, loginUser.password)
+    await generateJwtToken(res, nickname)
+    return res.status(200).send({ success: true })
+  }catch(err) {
+    return res.status(404).send({ success: false, errorMessage: err.message })
   }
-
-  const token = jwt.sign({ nickname: user.nickname }, SECRETKEY);
-
-  res.cookie('Authorization',`Bearer ${token}`)
-  return res.status(200).send({success: true})
-
 })
 
 export default router
